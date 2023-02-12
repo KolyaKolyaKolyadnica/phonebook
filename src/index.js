096d17d2a590511185e7f6e0c8609bd092d620b5
KolyaKolyaKolyadnica <borkovskihnikolaj@gmail.com>
2023-02-12 17:05:11 +0200

    test basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={reduxStore.store}>
       <PersistGate loading={null} persistor={reduxStore.persistor}>
         {/* <BrowserRouter basename="/react-hw-08-phonebook/"> */}
-        <BrowserRouter basename="/react-hw-08-phonebook.com/">
+        {/* <BrowserRouter basename="/react-hw-08-phonebook.com/"> */}
+        <BrowserRouter basename="/phonebook.com/">
           <App />
         </BrowserRouter>
       </PersistGate>
